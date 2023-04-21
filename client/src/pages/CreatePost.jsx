31c2c68cf682e30f1ff4b6f181b9dbcb9fe3ec07
nerdnightboy <nerdnightboy@.com>
2023-04-21 09:28:13 +0530

    Frontend inteface is ready now moving to backend

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -14,16 +14,21 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const generateImage = () => {
+    
+  }
+
   const handleSubmit = () => {
 
   }
 
   const handleChange = (e) => {
-
+    setForm({ ...form, [e.target.name]: e.target.value })
   }
 
   const handleSurpriseMe = () => {
-    
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt })
   }
 
   return (
@@ -39,13 +44,13 @@ const CreatePost = () => {
 
       <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-5'>
-          <FormField lableName="Your Name"
+          <FormField labelName="Your Name"
             type="text"
             name="name"
             placeholder="John Doe"
             value={form.name}
             handleChange={handleChange} />
-          <FormField lableName="Prompt"
+          <FormField labelName="Prompt"
             type="text"
             name="prompt"
             placeholder="3D render of a cute tropical fish in an aquarium on a dark blue background, digital art"
@@ -53,6 +58,36 @@ const CreatePost = () => {
             handleChange={handleChange}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe} />
+          <div className='relative bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+            {form.photo ? (
+              <img src={form.photo} alt={form.prompt} className='w-full h-full object-contain' />
+            )
+              : (
+                <img src={preview} alt='preview' className='w-9/2 h-9/2 object-contain opacity-40' />
+              )}
+
+            {generatingImg && (
+              <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className='mt-5 flex gap-5'>
+          <button type='button'
+            onClick={generateImage}
+            className='text-white bg-blue-500 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+
+          </button>
+        </div>
+
+        <div className='mt-10'>
+          <p className='mt-2 text-[#666e75] text-[14px]'>Have you created the image you want? You can share it with your friends, family and community.</p>
+          <button type='submit' className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>
+            {loading ? 'Sharing...' : 'Share with everyone'}
+          </button>
         </div>
       </form>
     </section>
