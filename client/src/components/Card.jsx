a21f5ee7bfb4b808c675986e47bd3f71cf8033b8
nerdnightboy <nerdnightboy@.com>
2023-04-21 15:55:25 +0530

    I think cloudinary is disturbing my code

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { downloadImage } from '../utils'
 
 const Card = () => {
   return (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { Loader, Card, FormField } from '../components/index';
 
-const RenderCards = ({data, title}) => {
+const RenderCards = ({ data, title}) => {
   if(data?.length > 0) {
-    return data.map((post) => <Card key={post.id} {...post} />)
+    return data.map((post) => <Card key={post._id} {...post} />)
   }
   return(
     <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
@@ -11,9 +11,33 @@ const RenderCards = ({data, title}) => {
 }
 
 const Home = () => {
-  const [loading, setLoadind] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
-  const [searchText, setSearchText] = useState('as');
+  const [searchText, setSearchText] = useState();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if(response.ok){
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchPosts();
+  }, []);
 
   return (
     <section className='max-w-7xl mx-auto'>
@@ -46,7 +70,7 @@ const Home = () => {
                 {searchText?(
                   <RenderCards data={[]} title="No search results found" />
                 ) : (
-                  <RenderCards data={[]} title="No posts found" />
+                  <RenderCards data={allPosts} title="No posts found" />
                 )
                 }
               </div>
